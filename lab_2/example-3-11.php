c540abfd1fd14e0a7e2103923fc619229d53241d
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-03 12:30:13 -0500

    Write out second example

---
+++ lab_2/example-3-11.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jason C. Nucciarone">
+    <meta name="copyright-holder" content="Oreilly Media">
+    <title>Example 3-11. Automatically converting a string to a number</title>
+    <link href="favicon.ico" rel="icon" type="image/x-icon">
+</head>
+<body>
+    <?php // Calculating the area of a circle with radius 5
+        $pi = "3.1414927";
+        $radius = 5;
+        echo $pi * ($radius * $radius);
+    ?>
+</body>
+</html>
