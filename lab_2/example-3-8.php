d3575b838b71b43664e76e6f16aea81b9f62c408
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-03 13:02:20 -0500

    Fix naming mistake

--- lab_2/example-3-8.php
+++ lab_2/example-3-8.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Jason C. Nucciarone">
     <meta name="copyright-holder" content="Oreilly Media">
-    <title>Example 3-8. Alternation multiline echo statement</title>
+    <title>Example 3-8. Alternative multiline echo statement</title>
     <link href="favicon.ico" rel="icon" type="image/x-icon">
 </head>
 <body>
