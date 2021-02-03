2c4cb78a56265382de036767a8ca4862c6319047
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-03 12:53:57 -0500

    Add challenge.php

---
+++ lab_2/challenge.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jason C. Nucciarone">
+    <title>Lab 2 Challenge</title>
+    <link href="favicon.ico" rel="icon" type="image/x-icon">
+</head>
+<body>
+    <?php
+        // Initialize seasons array
+        $seasons = array("Winter", "Spring", "Summer", "Autumn");
+
+        // echo Spring
+        echo $seasons[1];
+    ?>
+</body>
+</html>
