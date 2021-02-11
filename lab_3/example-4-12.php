f818dcb36a01fdacff4d2bebd3bc6ba9a7af3d1a
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-11 13:13:53 -0500

    Add first example for lab_3

---
+++ lab_3/example-4-12.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jason C. Nucciarone">
+    <meta name="copyright-holder" content="Oreilly Media">
+    <title>Example 4-12. Assigning a value and testing for equality</title>
+    <link href="favicon.ico" rel="icon" type="image/x-icon">
+</head>
+<body>
+    <?php
+        $month = "March";
+
+        if ($month == "March") echo "It's springtime!";
+    ?>
+</body>
+</html>
