9e78ba80e37282c77bb063640fef94345d988dc8
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-15 18:02:10 -0500

    Change location of class

--- lab_4/example-5-11.php
+++ lab_4/example-5-11.php
@@ -11,6 +11,16 @@
 </head>
 <body>
     <?php
+        class User
+        {
+            public $name, $password;
+
+            function save_user()
+            {
+                echo "Save User code goes here";
+            }
+        }
+        
         $object = new User;
         print_r($object);
         echo "<br>";
@@ -21,16 +31,6 @@
         echo "<br>";
 
         $object->save_user();
-
-        class User
-        {
-            public $name, $password;
-
-            function save_user()
-            {
-                echo "Save User code goes here";
-            }
-        }
     ?>
 </body>
 </html>
