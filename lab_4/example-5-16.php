96e15fbbb531011a0f8e718e6d58b0c965d8d86f
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-15 18:06:08 -0500

    Add fourth example to lab_4

--- lab_4/example-5-16.php
+++ lab_4/example-5-16.php
@@ -6,12 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Jason C. Nucciarone">
     <meta name="copyright-holder" content="Oreilly Media">
-    <title>Example 5-1. Three string functions</title>
+    <title>Example 5-16. Use the variable $this in a method</title>
     <link href="favicon.ico" rel="icon" type="image/x-icon">
 </head>
 <body>
     <?php
-        
+        class User
+        {
+            public $name, $password;
+
+            function get_password()
+            {
+                return $this->password;
+            }
+        }
+
+        $object = new User;
+        $object->password = "Who needs encryption anyways?";
+
+        echo $object->get_password();
     ?>
 </body>
 </html>
