1e65c8eadb1c41359340ee78e26187e6ac1fd1d2
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-15 18:01:51 -0500

    Change location of class

---
+++ lab_4/example-5-16.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jason C. Nucciarone">
+    <meta name="copyright-holder" content="Oreilly Media">
+    <title>Example 5-1. Three string functions</title>
+    <link href="favicon.ico" rel="icon" type="image/x-icon">
+</head>
+<body>
+    <?php
+        
+    ?>
+</body>
+</html>
