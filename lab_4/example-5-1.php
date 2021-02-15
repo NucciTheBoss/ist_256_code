619efa245ecc119fbab1bed64ab42d64132512f9
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-15 17:37:22 -0500

    Add first example for lab_4

---
+++ lab_4/example-5-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jason C. Nucciarone">
+    <meta name="copyright-holder" content="Oreilly Media">
+    <title>Example 5-1. Three string functions</title>
+    <link href="favicon.ico" rel="icon" type="image/x-icon">
+</head>
+<body>
+    <?php  // Some fancy functions for playing with strings
+        echo strrev(" !dlroW olleH");
+        echo str_repeat("Hip ", 2);
+        echo strtoupper("hooray!!!");
+    ?>
+</body>
+</html>
